1a10199dadc3f9173adaedf366ac42fc5c4c8a5c
behradsa <b.saraiyeh@gmail.com>
2024-09-26 18:04:55 +0330

    basename added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
+  { basename: "/parsa-saraiyeh-portfolio" },
 ]);
 
 createRoot(document.getElementById("root")).render(
