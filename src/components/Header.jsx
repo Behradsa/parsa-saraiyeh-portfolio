2f2e512dc187822a9d39db5bb1918fb8e307320c
behradsa <b.saraiyeh@gmail.com>
2024-10-04 20:54:41 +0330

    header logo import fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
+import parsa_logo_img from "../assets/parsa-logo.svg";
 function Header() {
   return (
     <header>
       <div className="logo-container">
-        <img src="/src/assets/parsa-logo.svg" alt="logo" />
+        <img src={parsa_logo_img} alt="logo" />
         <div>PARSA</div>
       </div>
       <div className="header-buttons-container">
