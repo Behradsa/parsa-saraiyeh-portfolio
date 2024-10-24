63a29a7d39e26e26336b516f54c2a4629357e882
Behradsa <b.saraiyeh@gmail.com>
2024-10-24 15:17:09 +0330

    root padding fixed

--- src/pages/CarriotPage.jsx
+++ src/pages/CarriotPage.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./CarriotPage.module.css";
-
+import Header from "../components/Header";
+import carriot_intro from "../assets/Carriot/carriot-intro.png";
 function CarriotPage() {
-  return <div>CarriotPage</div>;
+  return (
+    <div className={styles["page"]}>
+      <div className={styles["header-container"]}>
+        <Header />
+      </div>
+      <img
+        src={carriot_intro}
+        alt="Carriot"
+        className={styles["carriot-intro"]}
+      />
+    </div>
+  );
 }
 
 export default CarriotPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,19 @@ import "../components/MainGrid";
 import MainGrid from "../components/MainGrid";
 function HomePage() {
   return (
-    <div className="home-page-container">
-      <Header />
-      <MainGrid />
+    <div
+      style={{
+        width: "100%",
+        padding: "16px",
+        paddingTop: "0",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div className="home-page-container">
+        <Header />
+        <MainGrid />
+      </div>
     </div>
   );
 }
