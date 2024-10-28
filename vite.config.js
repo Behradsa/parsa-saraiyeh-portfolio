8667c450650977e2898ed88f5647c6976c54b88a
Behradsa <b.saraiyeh@gmail.com>
2024-10-28 12:40:56 +0330

    dockerized for production

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/parsa-saraiyeh-portfolio/",
+  // base: "/parsa-saraiyeh-portfolio/",
+  // plugins: [react()],
+  base: "/",
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
