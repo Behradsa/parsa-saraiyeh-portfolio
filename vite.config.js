33f01b1a78f1a55535d352d1e71df7bf03fb1ea8
saraiyeh-b <b.saraiyeh@gmail.com>
2025-11-06 18:54:09 +0330

    minor chage

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { imagetools } from "vite-imagetools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/parsa-saraiyeh-portfolio/",
+  base: "/",
   plugins: [react(), imagetools()],
   preview: {
     port: 3000,
