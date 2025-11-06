c7d8e5e195b5d91149c70533b28252a0f38eb0ed
saraiyeh-b <b.saraiyeh@gmail.com>
2025-11-06 17:30:14 +0330

    test image

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { imagetools } from "vite-imagetools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/parsa-saraiyeh-portfolio/",
   plugins: [react(), imagetools()],
   preview: {
     port: 3000,
